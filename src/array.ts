c4fe8b3a564859783f6ef3629259fe87f946730c
Kyohei Horikawa <kyohei3430@gmail.com>
2021-06-04 15:56:01 +0900

    Add array.ts

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings: Array<string> = ["tokyo", "osaka", "kyoto"];
+
+let strings2: string[] = ["ts", "js", "coffeescript"];
+let nijigen: number[][] = [
+  [10, 20],
+  [100, 1000],
+];
+
+let array: (string | number | boolean)[] = [1, "japan", false];
+// union types
